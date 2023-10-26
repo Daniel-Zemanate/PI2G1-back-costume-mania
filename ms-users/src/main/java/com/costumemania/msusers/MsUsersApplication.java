ada099e46db9337f49c0696f6e8c0ab08be1ad27
Daniel-Zemanate <zemanateaguirre@gmail.com>
2023-10-26 16:57:09 -0500

    PI2G1-3 Base configuration detailed for ms-users

--- ms-users/src/main/java/com/costumemania/msusers/MsUsersApplication.java
+++ ms-users/src/main/java/com/costumemania/msusers/MsUsersApplication.java
@@ -2,8 +2,10 @@ package com.costumemania.msusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsUsersApplication {
 
 	public static void main(String[] args) {
