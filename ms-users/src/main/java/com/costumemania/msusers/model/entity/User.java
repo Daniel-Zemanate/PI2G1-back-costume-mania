85f33dd93394850072bf2e30a6fc21b405511531
Daniel-Zemanate <zemanateaguirre@gmail.com>
2023-10-26 16:35:00 -0500

    Added User entity. Excluded attributes and changed  names due to ease keycloak integration

---
+++ ms-users/src/main/java/com/costumemania/msusers/model/entity/User.java
@@ -0,0 +1,49 @@
+package com.costumemania.msusers.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@SQLDelete(sql = "UPDATE users SET soft_delete = true WHERE id=?")
+@Where(clause = "soft_delete=false")
+@Table(name = "users", indexes = @Index(name = "unique_email", columnList = "email", unique = true))
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "status")
+    private Boolean status;
+
+    @Column(name = "soft_delete")
+    private Boolean softDelete = Boolean.FALSE;
+
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+
+}
