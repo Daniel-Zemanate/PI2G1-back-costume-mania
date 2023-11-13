2d1d6c006eb78cd74bb4f0d146f8489810db5fa9
Daniel-Zemanate <zemanateaguirre@gmail.com>
2023-11-13 00:51:12 -0500

    Keycloack with roles and jwt converter

--- ms-users/src/main/java/com/costumemania/msusers/configuration/security/SecurityConfig.java
+++ ms-users/src/main/java/com/costumemania/msusers/configuration/security/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.costumemania.msusers.configuration.security;
 
+import com.costumemania.msusers.configuration.security.jwt.JwtAuthenticationConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,21 +13,27 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationConverter jwtAuthenticationConverter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .securityMatcher("/api/v1/users/**")
                 .authorizeHttpRequests(httpRequests -> {
                     httpRequests
-                            .requestMatchers(HttpMethod.POST,"/users/crete").permitAll()
                             .anyRequest().authenticated();
                 })
                 .oauth2ResourceServer(oAuth -> {
-                    oAuth.jwt(jwtConfigurer -> {});
+                    oAuth.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter));
                 })
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
     }
+
 }
--- ms-users/src/main/java/com/costumemania/msusers/configuration/security/controller/TestController.java
+++ ms-users/src/main/java/com/costumemania/msusers/configuration/security/controller/TestController.java
@@ -1,16 +1,20 @@
 package com.costumemania.msusers.configuration.security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping(path = "/hello-1")
     public String helloAdmin() {
 
         return "Hello With Keycloak - ADMIN";
     }
+
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @GetMapping(path = "/hello-2")
     public String helloUser() {
 
