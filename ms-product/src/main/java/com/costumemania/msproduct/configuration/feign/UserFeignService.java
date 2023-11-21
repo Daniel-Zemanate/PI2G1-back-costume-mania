56bb3c70e969ba12b312f3c30f7f05fd32e9766d
Daniel-Zemanate <zemanateaguirre@gmail.com>
2023-11-21 09:08:52 -0500

    FIX compilation

--- ms-product/src/main/java/com/costumemania/msproduct/configuration/feign/UserFeignService.java
+++ ms-product/src/main/java/com/costumemania/msproduct/configuration/feign/UserFeignService.java
@@ -1,15 +1,10 @@
 package com.costumemania.msproduct.configuration.feign;
 
-import com.costumemania.msusers.model.dto.UserAccountResponse;
-import com.costumemania.msusers.model.entity.UserEntity;
-import jakarta.ws.rs.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
-public class UserFeignService{
+public class UserFeignService {
 
     @Autowired
     private UsersFeignRepository usersFeignRepository;
