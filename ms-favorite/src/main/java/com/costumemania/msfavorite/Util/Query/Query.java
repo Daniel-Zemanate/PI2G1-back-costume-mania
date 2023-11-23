eb56e192f0e7dada30641c645ab81d1f6b471581
Daniel-Zemanate <zemanateaguirre@gmail.com>
2023-11-22 20:40:37 -0500

    HF - query from FAV to fav

--- ms-favorite/src/main/java/com/costumemania/msfavorite/Util/Query/Query.java
+++ ms-favorite/src/main/java/com/costumemania/msfavorite/Util/Query/Query.java
@@ -5,6 +5,6 @@ package com.costumemania.msfavorite.Util.Query;
 public class Query {
 
 
-    public static final String query1 ="SELECT DISTINCT (model) FROM FAV ORDER BY model DESC";
+    public static final String query1 ="SELECT DISTINCT (model) FROM fav ORDER BY model DESC";
 
 }
