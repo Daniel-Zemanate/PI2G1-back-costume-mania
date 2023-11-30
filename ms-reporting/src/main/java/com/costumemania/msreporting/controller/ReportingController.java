14d3de5f0fddd47fd942872f1d11dba771a282ba
Ana Laura <sicz.ana@hotmail.com>
2023-11-30 02:31:16 -0300

    todos los PDFs funcionando

--- ms-reporting/src/main/java/com/costumemania/msreporting/controller/ReportingController.java
+++ ms-reporting/src/main/java/com/costumemania/msreporting/controller/ReportingController.java
@@ -40,6 +40,7 @@ public class ReportingController {
             return ResponseEntity.internalServerError().build();
         }
         // get sales with shipping
+        Collections.sort(sales, Comparator.comparing(Sale::getSaleDate));
         List<Sale> salesWithShipping = new ArrayList<>();
         for (Sale sale : sales) {
             if (sale.getShippingDate() != null) {
@@ -68,7 +69,7 @@ public class ReportingController {
 
     // average per month
     @GetMapping("/report1/{month}/{year}")
-    public ResponseEntity<AverageShippingTime> averageShippingTimeByMonth(@PathVariable int month, @PathVariable int year) {
+    public ResponseEntity<AverageAndSaleList> averageShippingTimeByMonth(@PathVariable int month, @PathVariable int year) {
         // get every sale
         List<Sale> sales = new ArrayList<>();
         String firstDay = year + "-" + month + "-01";
@@ -94,6 +95,7 @@ public class ReportingController {
             return ResponseEntity.internalServerError().build();
         }
         // get sales with shipping
+        Collections.sort(sales, Comparator.comparing(Sale::getSaleDate));
         List<Sale> salesWithShipping = new ArrayList<>();
         for (Sale sale : sales) {
             if (sale.getShippingDate() != null) {
@@ -110,17 +112,19 @@ public class ReportingController {
         }
         float average = (float) totalDays / salesWithShipping.size();
         double averageWith2Decimals = Math.round(average * 100) / 100.0;
-        AverageShippingTime result = new AverageShippingTime(salesWithShipping.get(0).getSaleDate().toLocalDate(),
+        AverageShippingTime shippingTime = new AverageShippingTime(salesWithShipping.get(0).getSaleDate().toLocalDate(),
                 salesWithShipping.get(salesWithShipping.size() - 1).getSaleDate().toLocalDate(),
                 sales.size(),
                 salesWithShipping.size(),
                 averageWith2Decimals);
+
+        AverageAndSaleList result = new AverageAndSaleList(shippingTime, salesWithShipping);
         return ResponseEntity.ok(result);
     }
 
     // average per customized period
     @GetMapping("/report1/dates/{firstDate}/{lastDate}")
-    public ResponseEntity<AverageShippingTime> averageShippingCustom(@PathVariable String firstDate, @PathVariable String lastDate) {
+    public ResponseEntity<AverageAndSaleList> averageShippingCustom(@PathVariable String firstDate, @PathVariable String lastDate) {
         // get every sale
         List<Sale> sales = new ArrayList<>();
         try {
@@ -137,6 +141,7 @@ public class ReportingController {
             return ResponseEntity.internalServerError().build();
         }
         // get sales with shipping
+        Collections.sort(sales, Comparator.comparing(Sale::getSaleDate));
         List<Sale> salesWithShipping = new ArrayList<>();
         for (Sale sale : sales) {
             if (sale.getShippingDate() != null) {
@@ -153,11 +158,13 @@ public class ReportingController {
         }
         float average = (float) totalDays / salesWithShipping.size();
         double averageWith2Decimals = Math.round(average * 100) / 100.0;
-        AverageShippingTime result = new AverageShippingTime(salesWithShipping.get(0).getSaleDate().toLocalDate(),
+        AverageShippingTime shippingTime = new AverageShippingTime(salesWithShipping.get(0).getSaleDate().toLocalDate(),
                 salesWithShipping.get(salesWithShipping.size() - 1).getSaleDate().toLocalDate(),
                 sales.size(),
                 salesWithShipping.size(),
                 averageWith2Decimals);
+
+        AverageAndSaleList result = new AverageAndSaleList(shippingTime, salesWithShipping);
         return ResponseEntity.ok(result);
     }
 
@@ -177,12 +184,12 @@ public class ReportingController {
         // if they are from same year, iterator per month
         if (firstMonth.getYear() == lastMonth.getYear()) {
             for (int i = firstMonth.getMonth(); i <= lastMonth.getMonth(); i++) {
-                ResponseEntity<AverageShippingTime> response = averageShippingTimeByMonth(i, firstMonth.getYear());
+                ResponseEntity<AverageAndSaleList> response = averageShippingTimeByMonth(i, firstMonth.getYear());
                 if (response.getStatusCode() == HttpStatus.OK) {
 
                     ShippingTimePeriod newPeriod = new ShippingTimePeriod(
                             i + "/" + lastMonth.getYear(),
-                            response.getBody().getAverageDelay());
+                            response.getBody().getAverageShippingTime().getAverageDelay());
                     result.add(newPeriod);
                 }
             }
@@ -191,11 +198,11 @@ public class ReportingController {
         // if they are from different year, iterator per year and month
         for (int j = firstMonth.getYear(); j <= lastMonth.getYear(); j++) {
             for (int i = 1; i <= 12; i++) {
-                ResponseEntity<AverageShippingTime> response = averageShippingTimeByMonth(i, j);
+                ResponseEntity<AverageAndSaleList> response = averageShippingTimeByMonth(i, j);
                 if (response.getStatusCode() == HttpStatus.OK) {
                     ShippingTimePeriod newPeriod = new ShippingTimePeriod(
                             i + "/" + j,
-                            response.getBody().getAverageDelay());
+                            response.getBody().getAverageShippingTime().getAverageDelay());
                     result.add(newPeriod);
                 }
             }
@@ -303,161 +310,57 @@ public class ReportingController {
                 saleDTOList,
                 averageAndSaleList);
     }
-}
-/*
 
     @GetMapping("/generatePdfReport/{month}/{year}")
     public ResponseEntity<byte[]> generatePdfReportPerMonth(@PathVariable int month, @PathVariable int year) {
-        // get every sale
-        List<Sale> sales = new ArrayList<>();
-        String firstDay = year + "-" + month + "-01";
-        // to generate last date
-        LocalDate nextMonth;
-        if (month == 12) {
-            nextMonth = LocalDate.of(year+1,1,1);
-        } else {
-            nextMonth = LocalDate.of(year,month+1,1);
+        // get info
+        ResponseEntity<AverageAndSaleList> averageAndSaleList = averageShippingTimeByMonth(month, year);
+        if (averageAndSaleList.getStatusCode() != HttpStatus.OK) {
+            return ResponseEntity.unprocessableEntity().build();
         }
-        LocalDate lastDay = nextMonth.minusDays(1);
-        try {
-            ResponseEntity respSales = saleService.getByDates(firstDay, String.valueOf(lastDay));
-            if (respSales.getStatusCode()== HttpStatus.OK) {
-                sales = (List<Sale>) respSales.getBody();
-                if (sales.isEmpty()) {
-                    return ResponseEntity.noContent().build();
-                }
-            } else {
-                return ResponseEntity.noContent().build();
-            }
-        } catch (FeignException e) {
-            return ResponseEntity.internalServerError().build();
-        }
-        // get sales with shipping
-        Collections.sort(sales, Comparator.comparing(Sale::getSaleDate));
-        List<Sale> salesWithShipping = new ArrayList<>();
-        for (Sale sale : sales) {
-            if (sale.getShippingDate()!=null) {
-                salesWithShipping.add(sale);
-            }
-        }
-        if (salesWithShipping.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-        // average days
-        long totalDays = 0;
-        for (Sale sale : salesWithShipping) {
-            totalDays += DAYS.between(sale.getSaleDate(), sale.getShippingDate());
-        }
-        float average = (float) totalDays /salesWithShipping.size();
-        double averageWith2Decimals = Math.round(average*100) / 100.0;
-
-        AverageShippingTime averageShippingResul = new AverageShippingTime(salesWithShipping.get(0).getSaleDate(),
-                salesWithShipping.get(salesWithShipping.size()-1).getSaleDate(),
-                sales.size(),
-                salesWithShipping.size(),
-                (float) averageWith2Decimals);
-
         // just deliveried sales
         List<SaleDTO> saleDTOList = new ArrayList<>();
-        for (Sale sale: salesWithShipping) {
-            saleDTOList.add(new SaleDTO(sale.getInvoice(),sale.getCatalog().getModel().getNameModel(),sale.getSaleDate().toLocalDate(),sale.getShippingDate().toLocalDate(), sale.getQuantity(),sale.getStatus().getStatus()));
+        for (Sale sale : averageAndSaleList.getBody().getSaleList()) {
+            saleDTOList.add(
+                    new SaleDTO(
+                            sale.getInvoice(),
+                            sale.getCatalog().getModel().getNameModel(),
+                            sale.getSaleDate().toLocalDate(),
+                            sale.getShippingDate().toLocalDate(),
+                            sale.getQuantity(),
+                            sale.getStatus().getStatus()));
         }
-
-        try {
-            // Load .jrxml file and compile into a JasperReport
-            JasperReport jasperReport = JasperCompileManager.compileReport("ms-reporting/src/main/resources/SaleByMonthShippingReport.jrxml");
-            // parameters
-            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(saleDTOList);
-            Map<String, Object> parameters = new HashMap<>();
-            parameters.put("createdBy", "Costume Mania");
-            parameters.put("firstDate", averageShippingResul.getFirstDate().toLocalDate());
-            parameters.put("lastDate", averageShippingResul.getLastDate().toLocalDate());
-            parameters.put("averageDelay", averageShippingResul.getAverageDelay());
-            parameters.put("quantitySales", averageShippingResul.getQuantitySales());
-            parameters.put("quantityDeliveredSales", averageShippingResul.getQuantityDeliveredSales());
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
-            //  configure the response to bytes in PDF
-            byte[] pdfBytes = JasperExportManager.exportReportToPdf(jasperPrint);
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_PDF);
-            headers.setContentDispositionFormData("inline", "salesReport.pdf");
-            return new ResponseEntity<>(pdfBytes, headers, org.springframework.http.HttpStatus.OK);
-        } catch (JRException e) {
-            return ResponseEntity.internalServerError().build();
-        }
+        // show PDF
+        return pdfGenerator(
+                "ms-reporting/src/main/resources/SaleByMonthShippingReport.jrxml",
+                saleDTOList,
+                averageAndSaleList.getBody());
     }
-
 
     // average per customized period
     @GetMapping("/generatePdfReport/dates/{firstDate}/{lastDate}")
-    public ResponseEntity<byte[]> generatePdfaverageShippingCustom (@PathVariable String firstDate, @PathVariable String lastDate){
-        // get every sale
-        List<Sale> sales = new ArrayList<>();
-        try {
-            ResponseEntity respSales = saleService.getByDates(firstDate, lastDate);
-            if (respSales.getStatusCode()== HttpStatus.OK) {
-                sales = (List<Sale>) respSales.getBody();
-                if (sales.isEmpty()) {
-                    return ResponseEntity.noContent().build();
-                }
-            } else {
-                return ResponseEntity.noContent().build();
-            }
-        } catch (FeignException e) {
-            return ResponseEntity.internalServerError().build();
+    public ResponseEntity<byte[]> generatePdfaverageShippingCustom(@PathVariable String firstDate, @PathVariable String lastDate) {
+        // get info
+        ResponseEntity<AverageAndSaleList> averageAndSaleList = averageShippingCustom(firstDate, lastDate);
+        if (averageAndSaleList.getStatusCode() != HttpStatus.OK) {
+            return ResponseEntity.unprocessableEntity().build();
         }
-        // get sales with shipping
-        Collections.sort(sales, Comparator.comparing(Sale::getSaleDate));
-        List<Sale> salesWithShipping = new ArrayList<>();
-        for (Sale sale : sales) {
-            if (sale.getShippingDate()!=null) {
-                salesWithShipping.add(sale);
-            }
-        }
-        if (salesWithShipping.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
-        // average days
-        long totalDays = 0;
-        for (Sale sale : salesWithShipping) {
-            totalDays += DAYS.between(sale.getSaleDate(), sale.getShippingDate());
-        }
-        float average = (float) totalDays /salesWithShipping.size();
-        double averageWith2Decimals = Math.round(average*100) / 100.0;
-
-        AverageShippingTime averageShippingResul = new AverageShippingTime(salesWithShipping.get(0).getSaleDate(),
-                salesWithShipping.get(salesWithShipping.size()-1).getSaleDate(),
-                sales.size(),
-                salesWithShipping.size(),
-                (float) averageWith2Decimals);
-
-        // just deliveried sales
+        // report deliveried sales
         List<SaleDTO> saleDTOList = new ArrayList<>();
-        for (Sale sale: salesWithShipping) {
-            saleDTOList.add(new SaleDTO(sale.getInvoice(),sale.getCatalog().getModel().getNameModel(),sale.getSaleDate().toLocalDate(),sale.getShippingDate().toLocalDate(), sale.getQuantity(),sale.getStatus().getStatus()));
+        for (Sale sale : averageAndSaleList.getBody().getSaleList()) {
+            saleDTOList.add(
+                    new SaleDTO(
+                            sale.getInvoice(),
+                            sale.getCatalog().getModel().getNameModel(),
+                            sale.getSaleDate().toLocalDate(),
+                            sale.getShippingDate().toLocalDate(),
+                            sale.getQuantity(),
+                            sale.getStatus().getStatus()));
         }
-
-        try {
-            // Load .jrxml file and compile into a JasperReport
-            JasperReport jasperReport = JasperCompileManager.compileReport("ms-reporting/src/main/resources/CustomSaleShippingReport.jrxml");
-            // parameters
-            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(saleDTOList);
-            Map<String, Object> parameters = new HashMap<>();
-            parameters.put("createdBy", "Costume Mania");
-            parameters.put("firstDate", averageShippingResul.getFirstDate().toLocalDate());
-            parameters.put("lastDate", averageShippingResul.getLastDate().toLocalDate());
-            parameters.put("averageDelay", averageShippingResul.getAverageDelay());
-            parameters.put("quantitySales", averageShippingResul.getQuantitySales());
-            parameters.put("quantityDeliveredSales", averageShippingResul.getQuantityDeliveredSales());
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
-            //  configure the response to bytes in PDF
-            byte[] pdfBytes = JasperExportManager.exportReportToPdf(jasperPrint);
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_PDF);
-            headers.setContentDispositionFormData("inline", "salesReport.pdf");
-            return new ResponseEntity<>(pdfBytes, headers, org.springframework.http.HttpStatus.OK);
-        } catch (JRException e) {
-            return ResponseEntity.internalServerError().build();
-        }
+        // show PDF
+        return pdfGenerator(
+                "ms-reporting/src/main/resources/CustomSaleShippingReport.jrxml",
+                saleDTOList,
+                averageAndSaleList.getBody());
     }
-*/
+}
